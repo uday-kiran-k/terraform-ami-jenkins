9563701e6d23741157f2021f35c7df5ecfffdfdb
Srinivasa Pavan Ganesh Vasa <113486368+ganeshvasa5@users.noreply.github.com>
2023-09-28 12:05:24 -0400

    Update script.sh

--- packer/script.sh
+++ packer/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install openjdk-17-jre -y
+sudo java --version 
 
 #install jenkins
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
@@ -10,13 +11,14 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
-sudo systemctl start jenkins.service
 
 #Install caddy
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https -y
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt-get update
+
+
 sudo apt-get install caddy -y
 
 sudo chmod 646 /etc/caddy/Caddyfile
@@ -26,6 +28,8 @@ jenkins.ganeshvasa.me, jenkins.banishmg.me, jenkins.udaykk.me {
 }
 EOF
 
+sudo systemctl start jenkins.service
 sudo systemctl enable --now caddy
 
+sudo systemctl status jenkins.service
 
