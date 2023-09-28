555db1c9ec385495294d542598ec5b43b0ddc4e4
Srinivasa Pavan Ganesh Vasa <113486368+ganeshvasa5@users.noreply.github.com>
2023-09-28 17:00:48 -0400

    Update script.sh

--- packer/script.sh
+++ packer/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install default-jre -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install default-jre -y
 sudo java --version 
 
 #install jenkins
